3bb11a6bdbad6a014ab06048ceab244f0f84bab0
lakshyaprajapat6990 <you@example.com>
2026-03-09 16:02:17 +0530

    done

--- src/services/CreatePoojaAPI.jsx
+++ src/services/CreatePoojaAPI.jsx
@@ -88,8 +88,8 @@ export const CreatePoojaAPI = async (data) => {
       data.items.forEach((b, i) => {
         formData.append(`items[${i}][title]`, b.title || "");
         formData.append(`items[${i}][titleHi]`, b.titleHi || "");
-        formData.append(`items[${i}[description]`, b.description || "");
-        formData.append(`items[${i}[descriptionHi]`, b.descriptionHi || "");
+        formData.append(`items[${i}][description]`, b.description || "");
+        formData.append(`items[${i}][descriptionHi]`, b.descriptionHi || "");
         formData.append(`items[${i}][price]`, b.price || "");
         formData.append(`items[${i}][image]`, b.img || "");
       });
